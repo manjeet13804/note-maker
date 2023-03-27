d264f5a3217d59a6d12fa4a42281cf262a09cfb9
manjeet13804 <51025738+manjeet13804@users.noreply.github.com>
2023-03-27 11:59:45 +0530

    Update logout.js

--- src/components/logout.js
+++ src/components/logout.js
@@ -1,7 +1,10 @@
 import "./logout.css"
+import { useNavigate } from "react-router-dom";
 const Logout = () => {
   const logoutHandle = ()=>{
+     const navigate =useNavigate()
     localStorage.clear();
+        navigate("/")
   }
   return (
     <button className="logout" onClick={logoutHandle}>Logout</button>
